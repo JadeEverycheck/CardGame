cb1e20bf0d17cf9c569a8e134db8d9117f06721c
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-18 16:44:10 +0200

    can see not hidden cards

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const HIDDEN_SYMBOL = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Card = ({ card, feedback, onClick }) => (
+  <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+    <span className="symbol">
+      {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+    </span>
+  </div>
+)
+
+const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
+
+class App extends React.Component {
+	handleCardClick(card) {
+	  console.log(card, 'clicked')
+	}
+  render() {
+    return (
+      <div className="memory">
+        <GuessCount guesses={0} />
+		<Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+		<Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+		<Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+		<Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+		<Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+		<Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
